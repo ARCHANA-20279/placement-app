f81e1416f3002116b0d10694ebbb239b16b6475b
ARCHANA-20279 <archana552m@gmail.com>
2026-07-11 12:21:30 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,21 @@ import './App.css'
 import Add from './component/Add'
 import Search from './component/Search'
 import Delete from './component/Delete'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ViewStud from './component/View'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Add />
-      <Search />
-      <Delete />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Add />}/>
+        <Route path='/search' element={<Search />}/>
+        <Route path='/delete' element={<Delete />}/>
+        <Route path='/view' element={<ViewStud />}/>
+        </Routes></BrowserRouter>
     </>
   )
 }
